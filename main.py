c9fa14d7bd2931c9b220f2e40c747f011d812ed6
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2022-08-22 06:30:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def init():
         await message.reply_text(config.PRIVATE_START_MESSAGE)
         
     @app.on_message(filters.command("donate") & filters.user(SUDO_USERS))
-async def donate_func(_, message: Message):
+    async def donate_func(_, message: Message):
       return await message.reply_sticker(
         sticker="CAACAgIAAx0CVBD5pAACN5VjAqGYgoyg-OXjf0lNy0lgmhH37wACIAADlp-MDqz9QTP0qm_5HgQ",
         caption="""""",
