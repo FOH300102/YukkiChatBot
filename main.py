b9be72659371cc738bd3894eb8d4c8a4d6336e1a
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2022-08-22 04:37:52 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -44,7 +44,21 @@ async def init():
             return
         await mongo.add_served_user(message.from_user.id)
         await message.reply_text(config.PRIVATE_START_MESSAGE)
-
+        
+    @Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_sticker(
+        sticker="CAACAgIAAx0CVBD5pAACN5VjAqGYgoyg-OXjf0lNy0lgmhH37wACIAADlp-MDqz9QTP0qm_5HgQ",
+        caption="""""",
+    reply_markup=InlineKeyboardMarkup(
+                [
+                    InlineKeyboardButton(
+                        "Endorsement", url="https://sociabuzz.com/iniokekkk/tribe"
+                       ),
+                  ]
+             )
+    )
+    
     @app.on_message(
         filters.command("mode") & filters.user(SUDO_USERS)
     )
