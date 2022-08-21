0c76c925ec1760114b1ced79be729d5ebb3cd8b5
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2022-08-22 05:05:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def init():
         await mongo.add_served_user(message.from_user.id)
         await message.reply_text(config.PRIVATE_START_MESSAGE)
         
-    @app.on_message(command("donate") & filters.private & ~filters.group)
+    @app.on_message(filters.command("donate") & filters.user(SUDO_USERS))
 async def donate.command(_, message: Message):
     await message.reply_sticker(
         sticker="CAACAgIAAx0CVBD5pAACN5VjAqGYgoyg-OXjf0lNy0lgmhH37wACIAADlp-MDqz9QTP0qm_5HgQ",
